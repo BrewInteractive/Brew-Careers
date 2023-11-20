bf1c55b2053a48d7becbd0e78978a9ae25867007
demirdanis <demir@brewww.com>
2023-11-20 20:58:26 +0300

    chore: unnecessary sections removed

--- apps/brew-careers/app/routes/_index.tsx
+++ apps/brew-careers/app/routes/_index.tsx
@@ -86,7 +86,7 @@ export default function Home() {
                   <span className="icon-bar"></span>
                 </button>
 
-                <div className="collapse navbar-collapse" id="navbar-collapse">
+                {/* <div className="collapse navbar-collapse" id="navbar-collapse">
                   <ul className="menu nav navbar-nav">
                     <li>
                       <a href="/">Home</a>
@@ -96,7 +96,7 @@ export default function Home() {
                       <a href="/#section-112289">Jobs</a>
                     </li>
                   </ul>
-                </div>
+                </div> */}
               </div>
             </nav>
             <div className="content container">
@@ -412,7 +412,7 @@ export default function Home() {
         </div>
       </div>
 
-      <footer className="container">
+      {/* <footer className="container">
         <a
           target="_blank"
           className="footer-logo"
@@ -429,7 +429,7 @@ export default function Home() {
           </span>
           <img src="images/logo__horizontal-95300540fb91da99f96dab9a76748ac00a0b39a5f02d432d373259194112dabb.svg" />
         </a>
-      </footer>
+      </footer> */}
     </body>
   );
 }
